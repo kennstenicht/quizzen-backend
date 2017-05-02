b4f1a6cdcfd3bfd669affd8240c2cde0d8793d8c
Christoph <mussichhaben@kennstenicht.de>
2017-05-02 14:56:54 +0200

    Feat(Seeds): Add question and answer seeds and create schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+q1 = Question.create(
+    label: 'Bezirke von Berlin',
+    date: DateTime.new(2017,4,13),
+    source: 'https://de.wikipedia.org/wiki/Berliner_Bezirke'
+  )
+q2 = Question.create(
+    label: 'Die längsten Flüsse in Deutschland',
+    date: DateTime.new(2017,4,13),
+    source: 'https://de.wikipedia.org/wiki/Liste_von_Fl%C3%BCssen_in_Deutschland'
+  )
+
+a1 = q1.answers.create(label: 'Neukölln')
+a2 = q2.answers.create(label: 'Donau', value: '2.811')
