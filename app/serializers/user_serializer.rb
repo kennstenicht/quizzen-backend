50bc8f551a7d7c2a1029264bb21b2d431f03e933
Christoph <christoph@ag-prop.com>
2020-12-13 11:35:27 +0100

    Fix(User): Fix email serialization

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,6 +5,6 @@ class UserSerializer < ApplicationSerializer
   attributes :nickname, :firstname, :lastname
 
   attribute :email, if: proc { |record, params|
-    params && params[:current_user].id == record.id
+    params[:current_user] && params[:current_user].id == record.id
   }
 end
