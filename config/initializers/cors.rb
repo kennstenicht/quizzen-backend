b507d6d8d2cbcd1c476a619578a21d8f5e2c297b
Christoph <christoph@ag-prop.com>
2021-04-17 15:55:02 +0200

    Fix(Cors): Add wuizzen.herokuapp.com to origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3000',
             'localhost:4100',
+            'https://quizzen.herokuapp.com',
+            'https://staging.quizzen.jetzt',
             'https://www.quizzen.jetzt'
 
     resource '*', headers: :any,
