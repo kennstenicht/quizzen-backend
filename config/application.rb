2118f1edff3d8b679cd5b6fb8830c7d427c68b8b
Christoph <christoph@ag-prop.com>
2021-04-17 16:37:55 +0200

    Chore(DefaultUrlOptions): Set url according to enviroment

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,5 @@ module QuizzenBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    Rails.application.routes.default_url_options = { host: 'http://localhost:3000' }
   end
 end
